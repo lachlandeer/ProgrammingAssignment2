817d550612897ffcb4a35f2108f8f7b5bed425b8
Lachlan Deer <lachlan.deer@gmail.com>
2015-12-24 11:46:48 +0100

    Described what function will do

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,18 @@
 # Source: rProgramming via Coursera and JHU,
 #         Programming Assigment 02
 #
+#
+# What this function does:
+#     - We want to cache the inverse of a matrix. To do this we
+#       work as follows:
+#         (i) define "makeCacheMatrix()"
+#             - A special matrix object that caches it inverse
+#         (ii) cacheSolve()
+#             - computes the inverse of a matrix returned from
+#               makeCacheMatrix().
+#             - An already cached matrix will have inverse retreived,
+#               otherwise computes the inverse
+#
 # Author(s) : Lachlan Deer (LD)
 #
 # Edit History:
