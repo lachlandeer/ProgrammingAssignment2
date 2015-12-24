4abb881facff552d42741f80938b9d2c677e66ef
Lachlan Deer <lachlan.deer@gmail.com>
2015-12-24 11:41:54 +0100

    Formatted function headers

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,17 @@
-## Put comments here that give an overall description of what your
+##################################################################
+#
+#          Caching the Inverse of a Matrix
+#
+# Source: rProgramming via Coursera and JHU,
+#         Programming Assigment 02
+#
+# Author(s) : Lachlan Deer (LD)
+#
+# Edit History:
+# 2015-12-24 - LD - Created File
+##################################################################
+
+
 ## functions do
 
 ## Write a short comment describing this function
